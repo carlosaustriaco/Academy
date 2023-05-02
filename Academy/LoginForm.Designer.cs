d20e7f14280ee7077bb7eac18c698d2bb96a1f6b
Carlos <carlos.aaustriaco@gmail.com>
2023-05-02 20:40:58 -0300

    Creating the initial interface creating the event that calls the process to exit

--- Academy/LoginForm.Designer.cs
+++ Academy/LoginForm.Designer.cs
@@ -28,19 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lblTitle = new Label();
+            lblEmail = new Label();
+            lblPassword = new Label();
+            tbEmail = new TextBox();
+            tbPassword = new TextBox();
+            btnLogin = new Button();
+            lblCancel = new Button();
             SuspendLayout();
             // 
-            // Form1
+            // lblTitle
+            // 
+            lblTitle.Anchor = AnchorStyles.None;
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTitle.Location = new Point(83, 14);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(165, 30);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Academy Login";
+            lblTitle.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(43, 80);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(44, 15);
+            lblEmail.TabIndex = 1;
+            lblEmail.Text = "E-mail:";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(27, 123);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(60, 15);
+            lblPassword.TabIndex = 2;
+            lblPassword.Text = "Password:";
+            // 
+            // tbEmail
+            // 
+            tbEmail.Location = new Point(101, 77);
+            tbEmail.Name = "tbEmail";
+            tbEmail.Size = new Size(174, 23);
+            tbEmail.TabIndex = 3;
+            // 
+            // tbPassword
+            // 
+            tbPassword.Location = new Point(101, 120);
+            tbPassword.Name = "tbPassword";
+            tbPassword.Size = new Size(174, 23);
+            tbPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(200, 182);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblCancel
+            // 
+            lblCancel.Location = new Point(119, 182);
+            lblCancel.Name = "lblCancel";
+            lblCancel.Size = new Size(75, 23);
+            lblCancel.TabIndex = 6;
+            lblCancel.Text = "Cancel";
+            lblCancel.UseVisualStyleBackColor = true;
+            lblCancel.Click += lblCancelClick;
+            // 
+            // LoginForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
+            BackColor = Color.White;
+            ClientSize = new Size(334, 230);
+            Controls.Add(lblCancel);
+            Controls.Add(btnLogin);
+            Controls.Add(tbPassword);
+            Controls.Add(tbEmail);
+            Controls.Add(lblPassword);
+            Controls.Add(lblEmail);
+            Controls.Add(lblTitle);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "LoginForm";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTitle;
+        private Label lblEmail;
+        private Label lblPassword;
+        private TextBox tbEmail;
+        private TextBox tbPassword;
+        private Button btnLogin;
+        private Button lblCancel;
     }
 }
--- Academy/LoginForm.cs
+++ Academy/LoginForm.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Academy
 {
     public partial class LoginForm : Form
@@ -10,6 +12,23 @@ namespace Academy
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        /// <summary>
+        /// Event of clicking on the canceling button.
+        /// <paramref name="e"/>
+        /// <paramref name="sender"/>
+        /// </summary>
+        /// <param name="e">Base class of the information o the event.</param>
+        /// <param name="sender">The object that calls this event.</param>
+        private void lblCancelClick(object sender, EventArgs e)
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+
+            if (currentProcess != null)
+            {
+                currentProcess.CloseMainWindow();
+            }
         }
     }
 }
